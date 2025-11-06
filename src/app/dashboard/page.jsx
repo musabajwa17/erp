0cade0601ad26a35d7a718e749d958cbe7bdcab5
Muhammad Adrees <adreesservicesproviders@gmail.com>
2025-11-06 17:39:11 +0500

    new changes 6 nov

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,14 +1,13 @@
-"use client";
+"use client"
 import Sidebar from '../../components/sidebar/Sidebar';
 import ProtectedPage from '../../components/contact/ProtectedPage/AuthorizedPage';
-import Content from '../../components/dashboard/Content';
 function Dashboard() {
   return (
     <ProtectedPage>
-    <div className=' flex'>
-        <Sidebar/>
-    </div>
-     </ProtectedPage>
+      <div className='flex'>
+        <Sidebar />
+      </div>
+    </ProtectedPage>
   );
 }
 
